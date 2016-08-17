1476d82aa66628143ea322138593bb7707d65b7e
hoctor.meng <hoctor.meng@sao.so>
2016-08-17 11:27:38 +0800

    新增jackson注解

--- src/main/java/com/xinma/base/tag/CloudTag.java
+++ src/main/java/com/xinma/base/tag/CloudTag.java
@@ -1,5 +1,11 @@
 package com.xinma.base.tag;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * 二维码标签实体类
  * 
@@ -8,38 +14,44 @@ package com.xinma.base.tag;
  * @date 2016年6月4日
  *
  */
+@JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CloudTag {
 
 	/**
 	 * 标签编码版本，不同版本的标签编码的方式‘编码后code的长度以及字符范围不同
 	 */
+	@JsonProperty("vr")
 	private Byte codeVersion;
-	
+
 	/**
 	 * 标签系统唯一码，该码必须系统内唯一，不同version的序列号也不能相同，取值范围0 ~ 2^64
 	 */
+	@JsonProperty("id")
 	private Long tagId;
-	
+
 	/**
 	 * 系统标签主入口,二维码标签的编码值
 	 */
+	@JsonProperty("hc")
 	private String hiddenCode;
-	
+
 	/**
 	 * 编码时用于生成hiddenCode的随机数
 	 */
+	@JsonProperty("ha")
 	private Long hiddenRandomCode;
-	
-	
+
 	/**
 	 * 系统标签备用码，如果hiddenCode失效，使用displayCode查询
 	 */
+	@JsonProperty("dc")
 	private String displayCode;
 
-	
 	/**
 	 * 编码时用于生成displayCode的随机数
 	 */
+	@JsonProperty("da")
 	private Long displayRandomCode;
 
 	public Byte getCodeVersion() {
@@ -89,7 +101,8 @@ public class CloudTag {
 	public void setDisplayRandomCode(Long displayRandomCode) {
 		this.displayRandomCode = displayRandomCode;
 	}
-	
+
+	@JsonIgnore
 	public boolean isValid() {
 		return this.tagId != null && this.hiddenRandomCode != null;
 	}
