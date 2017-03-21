f4402408cc8cf1f182397ef53d4eb067cf256ae9
Alauda <hoctor.meng@gmail.com>
2017-03-21 23:44:47 +0800

    refactor with springboot

--- src/main/java/com/xinma/base/tag/TagConstants.java
+++ src/main/java/com/xinma/base/tag/TagConstants.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 
 /**
  * 云码标签类库常量定义类，该类中定义的常量只增加不可修改
+ * 
  * @author Alauda
  *
  * @date 2016年6月6日
@@ -14,7 +15,7 @@ public class TagConstants {
 	/**************************************
 	 ****** 标签编码版本
 	 *************************************/
-	
+
 	/**
 	 * 数字与字幕混合编码，编码后字符串长度24
 	 */
@@ -25,54 +26,56 @@ public class TagConstants {
 	public static final byte CODE_VERSION_5 = 0x03;
 	public static final byte CODE_VERSION_6 = 0x02;
 	public static final byte CODE_VERSION_7 = 0x01;
-	
+
 	protected static final byte VERSION_1_HIDDEN_CODE_LEN = 24;
 	protected static final byte VERSION_1_DISPLAY_CODE_LEN = 16;
-	
+
 	/**
 	 * 64bit序号最大值
 	 */
 	protected static final BigInteger maskOf64Bit = new BigInteger("FFFFFFFFFFFFFFFF", 16);
-	
+
 	/**
 	 * 114bit序号最大值
 	 */
 	protected static final BigInteger maskOf114Bit = new BigInteger("3FFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
-	
+
 	/**
 	 * 82bit序号最大值
 	 */
 	protected static final BigInteger maskOf82Bit = new BigInteger("3FFFFFFFFFFFFFFFFFFFF", 16);
-			
 
 	protected static final BigInteger codeTable36Length = BigInteger.valueOf(36);
 	protected static final BigInteger codeTable40Length = BigInteger.valueOf(40);
 
 	// 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$-_+!*'(),.
-	
+
 	/**
 	 * 该字符表用于纯数字格式字符串编码
 	 */
-	protected static final char codeTableDigital[] = { '4', '7', '8', '6', '2', '5', '1', '0',  '9', '3' };
+	protected static final char codeTableDigital[] = { '4', '7', '8', '6', '2', '5', '1', '0', '9', '3' };
 
 	/**
-	 * 该字符表用于displayCode编码
+	 * 该字符表用于displayCode编码, 由数字0~9 ，大写英文字母(I O 除外)，特殊字符 + - 组成
 	 */
-	protected static final char codeTable36[] = { 'E', 'B', 'A','S', 'M', '5', 'F', 'G', 'C',
-			'L', 'H',	'Z', '-', 'D', '4', '1', 'W', '3', 'T', 'P', 'Y', '9', '8', 'R', 'J',
-			'2', '+', '0', '7', 'N', 'X', 'V', 'Q', '6', 'K', 'U' };
+	protected static final char codeTable36[] = { 'E', 'B', 'A', 'S', 'M', '5', 'F', 'G', 'C', 'L', 'H', 'Z', '-', 'D',
+			'4', '1', 'W', '3', 'T', 'P', 'Y', '9', '8', 'R', 'J', '2', '+', '0', '7', 'N', 'X', 'V', 'Q', '6', 'K',
+			'U' };
 
 	/**
 	 * 该字符表用于CODE_VERSION_1的hiddenCode编码
 	 */
-	protected static final char codeTable40[] = { 'C', '5', 'K', 'Y', 'G', 'R', 'F', 'E', 'T', '6', 'V',
-			'*', 'H', 'Q', '1', 'B', 'A', '4', '0', '9', 'X', 'U', '8', '-', 'P', 'W', '7', 'D', 'I',
-			'!', 'L', 'Z', '$', 'O', 'J', 'N', '2', 'M', 'S', '3' };
+	protected static final char codeTable40[] = { 'C', '5', 'K', 'Y', 'G', 'R', 'F', 'E', 'T', '6', 'V', '*', 'H', 'Q',
+			'1', 'B', 'A', '4', '0', '9', 'X', 'U', '8', '-', 'P', 'W', '7', 'D', 'I', '!', 'L', 'Z', '$', 'O', 'J',
+			'N', '2', 'M', 'S', '3' };
 
 	/**
 	 * 查找某一字符在对应字符表的索引值
-	 * @param ch 要查找的字符
-	 * @param lookupTable 要查找的字符表
+	 * 
+	 * @param ch
+	 *            要查找的字符
+	 * @param lookupTable
+	 *            要查找的字符表
 	 * @return 字符在表中的索引值
 	 */
 	public static byte getTableIndex(char ch, char[] lookupTable) {
@@ -80,10 +83,10 @@ public class TagConstants {
 			if (lookupTable[i] == ch)
 				return (byte) i;
 		}
-		
+
 		throw new IllegalArgumentException("illegal argument of ch.");
 	}
-	
+
 	/**
 	 * 按字节相异或，计算校验字节
 	 * 
@@ -94,13 +97,14 @@ public class TagConstants {
 
 	/**
 	 * BCC(Block check Character)异或校验方法
+	 * 
 	 * @param data
 	 * @return 字节校验码
 	 */
 	public static int bccByteCode(BigInteger data) {
 		int i = 0;
 		int byteCode = 0;
-		
+
 		while (i < data.bitLength()) {
 			byteCode ^= (data.shiftRight(i).intValue() & 0xFF);
 			i += 8;
--- src/test/java/com/xinma/base/tag/TagBuilderTest.java
+++ src/test/java/com/xinma/base/tag/TagBuilderTest.java
@@ -2,16 +2,16 @@ package com.xinma.base.tag;
 
 import java.math.BigInteger;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TagBuilderTest {
 	@Test
-	  public void codeVersion1Test() throws Exception {
+	public void codeVersion1Test() throws Exception {
 		BigInteger startUniqueCode = BigInteger.valueOf(10101020);
 		long tagCount = 1000000;
-		TagBuilder builder = new TagBuilder(TagConstants.CODE_VERSION_1,startUniqueCode, tagCount);
-		
+		TagBuilder builder = new TagBuilder(TagConstants.CODE_VERSION_1, startUniqueCode, tagCount);
+
 		CloudTag cloudTag = builder.nextCloudTag();
 		while (cloudTag != null) {
 			CloudTag decodeTag = TagParser.decodeTagByHiddenCode(cloudTag.getHiddenCode());
@@ -19,14 +19,16 @@ public class TagBuilderTest {
 			Assert.assertEquals(cloudTag.getCodeVersion(), decodeTag.getCodeVersion());
 			Assert.assertEquals(cloudTag.getHiddenCode(), decodeTag.getHiddenCode());
 			Assert.assertEquals(cloudTag.getHiddenRandomCode(), decodeTag.getHiddenRandomCode());
-			//System.out.println(cloudTag.getUniqueCode() + "," + cloudTag.getHiddenCode() + "," + cloudTag.getDisplayCode() + "," + cloudTag.getHiddenRandomCode());
+			// System.out.println(cloudTag.getUniqueCode() + "," +
+			// cloudTag.getHiddenCode() + "," + cloudTag.getDisplayCode() + ","
+			// + cloudTag.getHiddenRandomCode());
 			decodeTag = TagParser.decodeTagByDisplayCode(cloudTag.getDisplayCode());
 			Assert.assertEquals(cloudTag.getTagId(), decodeTag.getTagId());
 			Assert.assertEquals(cloudTag.getCodeVersion(), decodeTag.getCodeVersion());
 			Assert.assertEquals(cloudTag.getDisplayCode(), decodeTag.getDisplayCode());
 			Assert.assertEquals(cloudTag.getDisplayRandomCode(), decodeTag.getDisplayRandomCode());
-			
+
 			cloudTag = builder.nextCloudTag();
 		}
-	  }
+	}
 }
